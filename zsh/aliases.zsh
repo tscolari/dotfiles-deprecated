4fe46d259cd66702f8c12a387504fc0ac51437ff
Tiago Scolari <tscolari@housetrip.com>
2013-08-08 11:01:02 +0100

    Changing some configuration
    * removing unwanted spaces before saving in view
    * removing bad gem sources
    * binding C^bb to work as C^aa works in screen
    * alias for killing the last foreground job

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -152,3 +152,5 @@ alias rs='rspec spec'
 alias sp='sprintly'
 # spb = sprintly branch - create a branch automatically based on the bug you're working on
 alias spb="git checkout -b \`sp | tail -2 | grep '#' | sed 's/^ //' | sed 's/[^A-Za-z0-9 ]//g' | sed 's/ /-/g' | cut -d"-" -f1,2,3,4,5\`"
+
+alias kl="kill -9 %1"
