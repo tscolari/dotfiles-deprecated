6ecac9585d6677e6d4a1e66ecd125901bd2a148f
Tiago Scolari <tscolari@gmail.com>
2017-06-05 20:43:47 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -4,18 +4,22 @@ cd $(dirname $0)
 DOTFILES_FOLDER=$(pwd)
 
 PPAS="
+  ppa:aacebedo/fasd
+  ppa:fkrull/deadsnakes
   ppa:git-core/ppa
   ppa:neovim-ppa/stable
-  ppa:fkrull/deadsnakes
-  ppa:pi-rho/dev
-  ppa:tmate.io/archive
-  ppa:aacebedo/fasd
-  ppa:noobslab/themes
   ppa:noobslab/icons
+  ppa:noobslab/themes
+  ppa:numix/ppa
+  ppa:pi-rho/dev
+  ppa:snwh/pulp
+  ppa:tista/adapta
+  ppa:tmate.io/archive
 "
 
 PACKAGES="
   ack-grep
+  adapta-gtk-theme
   build-essential
   clipit
   cmake
@@ -40,7 +44,11 @@ PACKAGES="
   libyaml-dev
   neovim
   nodejs
+  numix-gtk-theme
   openssl
+  paper-cursor-theme
+  paper-gtk-theme
+  paper-icon-theme
   pinentry-curses
   python3-pip
   python3.6
@@ -68,32 +76,34 @@ GO_BINS="
 "
 
 function main {
-	fetch_submodules
+  fetch_submodules
 
-	pre_apt_get
-	add_ppas
-	apt_get_stuff
+  pre_apt_get
+  add_ppas
+  apt_get_stuff
 
-	install_go
-	install_zsh
-	install_git
-	install_psql
-	install_ruby
-	install_inputrc
-	install_tmux
-	install_vimlocal
+  install_vagrant
+  install_go
+  install_zsh
+  install_git
+  install_psql
+  install_ruby
+  install_inputrc
+  install_tmux
+  install_vimlocal
+  install_gnome_themes
 }
 
 function fetch_submodules {
-	git submodule update --init --recursive
+  git submodule update --init --recursive
 }
 
 function install_go {
   (
-    cd /tmp
-    wget https://godeb.s3.amazonaws.com/godeb-amd64.tar.gz
-    tar xzf godeb-amd64.tar.gz
-    ./godeb install || true
+  cd /tmp
+  wget https://godeb.s3.amazonaws.com/godeb-amd64.tar.gz
+  tar xzf godeb-amd64.tar.gz
+  ./godeb install || true
   )
 }
 
@@ -107,8 +117,8 @@ function install_go_bins {
 function pre_apt_get {
   sudo apt-get update
   sudo apt-get install -y \
-     software-properties-common \
-     ca-certificates
+    software-properties-common \
+    ca-certificates
 }
 
 function add_ppas {
@@ -128,68 +138,96 @@ function apt_get_stuff {
 }
 
 function install_zsh {
-	runcoms_folder=$DOTFILES_FOLDER/zsh/prezto/runcoms
-	for file in $(ls $runcoms_folder/z*)
-	do
-		target=$HOME/.$(basename ${file:t})
-		rm -rf $target
-		ln -s $file $target
-	done
+  runcoms_folder=$DOTFILES_FOLDER/zsh/prezto/runcoms
+  for file in $(ls $runcoms_folder/z*)
+  do
+    target=$HOME/.$(basename ${file:t})
+    rm -rf $target
+    ln -s $file $target
+  done
 
-	rm -rf $HOME/.zshrc
-	rm -rf $HOME/.zprezto
-	rm -rf $HOME/.zpreztorc
-	ln -s $DOTFILES_FOLDER/zsh/zshrc $HOME/.zshrc
-	ln -s $DOTFILES_FOLDER/zsh/zpreztorc $HOME/.zpreztorc
-	ln -s $DOTFILES_FOLDER/zsh/prezto $HOME/.zprezto
+  rm -rf $HOME/.zshrc
+  rm -rf $HOME/.zprezto
+  rm -rf $HOME/.zpreztorc
+  ln -s $DOTFILES_FOLDER/zsh/zshrc $HOME/.zshrc
+  ln -s $DOTFILES_FOLDER/zsh/zpreztorc $HOME/.zpreztorc
+  ln -s $DOTFILES_FOLDER/zsh/prezto $HOME/.zprezto
 }
 
 function install_git {
-	rm -rf $HOME/.gitconfig
-	rm -rf $HOME/.gitignore
-	ln -s $DOTFILES_FOLDER/git/gitconfig $HOME/.gitconfig
-	ln -s $DOTFILES_FOLDER/git/gitignore $HOME/.gitignore
+  rm -rf $HOME/.gitconfig
+  rm -rf $HOME/.gitignore
+  ln -s $DOTFILES_FOLDER/git/gitconfig $HOME/.gitconfig
+  ln -s $DOTFILES_FOLDER/git/gitignore $HOME/.gitignore
 }
 
 function install_psql {
-	rm -rf $HOME/.psqlrc
-	ln -s $DOTFILES_FOLDER/psqlrc $HOME/.psqlrc
+  rm -rf $HOME/.psqlrc
+  ln -s $DOTFILES_FOLDER/psqlrc $HOME/.psqlrc
 
-	sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" > "/etc/apt/sources.list.d/pgdg.list"'
-	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
-	  sudo apt-key add -
-	sudo apt update
-	sudo apt install -y postgresql-9.6 libpq-dev postgresql-contrib-9.6
+  sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" > "/etc/apt/sources.list.d/pgdg.list"'
+  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
+    sudo apt-key add -
+  sudo apt update
+  sudo apt install -y postgresql-9.6 libpq-dev postgresql-contrib-9.6
 }
 
 function install_ruby {
-	rm -rf $HOME/.gemrc
-	rm -rf $HOME/.pryrc
-	ln -s $DOTFILES_FOLDER/ruby/gemrc $HOME/.gemrc
-	ln -s $DOTFILES_FOLDER/ruby/pryrc $HOME/.pryrc
+  rm -rf $HOME/.gemrc
+  rm -rf $HOME/.pryrc
+  ln -s $DOTFILES_FOLDER/ruby/gemrc $HOME/.gemrc
+  ln -s $DOTFILES_FOLDER/ruby/pryrc $HOME/.pryrc
 
-	curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
-	curl -sSL https://get.rvm.io | bash
+  curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+  curl -sSL https://get.rvm.io | bash
+  rvm get stable --auto-dotfiles
 }
 
 function install_inputrc {
-	rm -rf $HOME/.inputrc
-	rm -rf $HOME/.editrc
-	ln -s $DOTFILES_FOLDER/vimify/inputrc $HOME/.inputrc
-	ln -s $DOTFILES_FOLDER/vimify/editrc $HOME/.editrc
+  rm -rf $HOME/.inputrc
+  rm -rf $HOME/.editrc
+  ln -s $DOTFILES_FOLDER/vimify/inputrc $HOME/.inputrc
+  ln -s $DOTFILES_FOLDER/vimify/editrc $HOME/.editrc
 }
 
 function install_tmux {
-	rm -rf $HOME/.tmux.conf
-	rm -rf $HOME/.tmux
-	ln -s $DOTFILES_FOLDER/tmux $HOME/.tmux
-	ln -s $HOME/.tmux/tmux.conf $HOME/.tmux.conf
-	sudo ln -s $(which tmux-next) /usr/local/bin/tmux
+  rm -rf $HOME/.tmux.conf
+  rm -rf $HOME/.tmux
+  ln -s $DOTFILES_FOLDER/tmux $HOME/.tmux
+  ln -s $HOME/.tmux/tmux.conf $HOME/.tmux.conf
+  sudo ln -s $(which tmux-next) /usr/local/bin/tmux
+}
+
+function install_vagrant {
+  wget -O /tmp/vagrant.deb https://releases.hashicorp.com/vagrant/1.9.4/vagrant_1.9.4_x86_64.deb
+  sudo dpkg -i /tmp/vagrant.deb
+  rm /tmp/vagrant.deb
 }
 
 function install_vimlocal {
-	rm -rf $HOME/.vimrc.local
-	ln -s $DOTFILES_FOLDER/vim/vimrc.local $HOME/.vimrc.local
+  rm -rf $HOME/.vimrc.local
+  ln -s $DOTFILES_FOLDER/vim/vimrc.local $HOME/.vimrc.local
+}
+
+function install_gnome_themes {
+  install_gnome_theme https://github.com/mgyugcha/copernico-theme
+  install_gnome_theme https://github.com/horst3180/Arc-theme
+  install_gnome_theme https://github.com/luisvilches/uranus-ligth-theme-gnome-shell-3.20
+}
+
+function install_gnome_theme {
+  local repo=$1
+  local folder_name=$(basename $1)
+
+  if [ ! -d ~/.themes/$folder_name ]
+  then
+    git clone $repo ~/.themes/$folder_name
+  else
+    (
+      cd ~/.themes/$folder_name
+      git pull origin master
+    )
+  fi
 }
 
 main
